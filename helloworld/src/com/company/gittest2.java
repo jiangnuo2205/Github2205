8954cd73d6bb89717ebe9dda3942f506f535f0cf
jiangnuo2205 <jiangnuo2205@163.com>
2022-06-01 16:49:37 +0800

    这是第二次提交commit，但未显示更新内容

---
+++ helloworld/src/com/company/gittest2.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class gittest2 {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("gittest2测试成功");
+       
+    }
+}
