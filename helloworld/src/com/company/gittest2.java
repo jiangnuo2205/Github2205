17dec6d5b0571a08af1619db6d0427eb1899d7e0
jiangnuo2205 <jiangnuo2205@163.com>
2022-06-01 18:26:35 +0800

    第四次全程复习

--- helloworld/src/com/company/gittest2.java
+++ helloworld/src/com/company/gittest2.java
@@ -5,5 +5,8 @@ public class gittest2 {
         // write your code here
         System.out.println("gittest2测试成功");
         System.out.println("只添加一行字输出，第三次测试，用sourcetree");
+        System.out.println("哈哈哈,第四次用来巩固复习");
+
+
     }
 }
