199fd39e31f838062ba6ebc4fdac7bde4f9812de
jiangnuo2205 <jiangnuo2205@163.com>
2022-06-01 17:01:53 +0800

    第三次，用sourcetree提交

--- helloworld/src/com/company/gittest2.java
+++ helloworld/src/com/company/gittest2.java
@@ -4,6 +4,6 @@ public class gittest2 {
     public static void main(String[] args) {
         // write your code here
         System.out.println("gittest2测试成功");
-       
+        System.out.println("只添加一行字输出，第三次测试，用sourcetree");
     }
 }
