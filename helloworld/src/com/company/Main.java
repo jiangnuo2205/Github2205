fb33709879da3ca560add0ff46c12bd822cfa008
jiangnuo2205 <106654271+jiangnuo2205@users.noreply.github.com>
2022-06-03 20:01:45 +0800

    Update Main.java

--- helloworld/src/com/company/Main.java
+++ helloworld/src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("hello-world");
         System.out.println("测试git");
+	    System.out.println("是的，我在测试从远程库下载到本地库");
     }
 }
